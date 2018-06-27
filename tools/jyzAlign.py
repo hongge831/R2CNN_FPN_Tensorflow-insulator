28b78671021afcd420ad8e705d2e26a2f87a6c9e
hongge831 <541324951@qq.com>
2018-06-27 22:06:38 +0800

    add tools

---
+++ tools/jyzAlign.py
@@ -0,0 +1,60 @@
+import os
+import cv2
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+src = 'D:/jyzdata/jyzbig/'
+fileList = os.listdir(src)
+jsonList = [x for x in fileList if x.endswith('json')]
+# temp = jsonList[2]
+
+
+def align(num,filename):
+    src = 'D:/jyzdata/jyzbig/'
+    desrc = 'D:/jyzdata/jyzAlign/'
+    with open(src + filename, 'r') as f:
+        jsonfile = json.loads(f.read())
+        img = cv2.imread(src + jsonfile['imagePath'])
+        for idx in range(len(jsonfile['shapes'])):
+            cnt = np.asarray(jsonfile['shapes'][idx]['points'])
+            cnt = cnt.astype(int)
+            rect = cv2.minAreaRect(cnt)
+            box = cv2.boxPoints(rect)  # cv2.boxPoints(rect) for OpenCV 3.x 获取最小外接矩形的4个顶点
+            box = np.int0(box)
+            # print(str(idx) + src + jsonfile['imagePath'])
+            # cv2.circle(img, (box[0][0], box[0][1]), 50, (255, 0, 0), 10)  # 修改最后一个参数
+            # cv2.circle(img, (box[1][0], box[1][1]), 50, (0, 255, 0), 10)  # 修改最后一个参数
+            # cv2.circle(img, (box[2][0], box[2][1]), 50, (0, 0, 255), 10)  # 修改最后一个参数
+            # cv2.circle(img, (box[3][0], box[3][1]), 50, (0, 0, 0), 10)  # 修改最后一个参数
+            if (rect[1][0]<rect[1][1]):
+                RotateMatrix = cv2.getRotationMatrix2D(center=(box[0][0], box[0][1]), angle=(90 + rect[2]), scale=1)
+                if(abs(rect[2])>50):
+                    addition = rect[1][1]//3
+                else:
+                    addition = rect[1][1]
+                RotateMatrix[0, 2] += addition
+                # RotateMatrix[1, 2] += rect[1][1]
+                RotImg = cv2.warpAffine(img, RotateMatrix, (img.shape[0] * 2, img.shape[1]))  # 为了不让尺寸的图像部分消失
+                y1 = box[0][1]
+                y0 = y1 - rect[1][0]
+                x0 = box[0][0]+addition
+                x1 = x0 - rect[1][1]
+                cropImg = RotImg[int(y0):int(y1), int(x1):int(x0)]
+                cv2.imwrite(desrc + str(num) +str(idx) + '_' + jsonfile['shapes'][idx]['label'] + '.jpg', cropImg)
+            else:
+                RotateMatrix = cv2.getRotationMatrix2D(center=(box[0][0], box[0][1]), angle=rect[2], scale=1)
+                RotImg = cv2.warpAffine(img, RotateMatrix, (img.shape[0] * 2, img.shape[1]))  # 为了不让尺寸的图像部分消失
+                y0 = box[0][1] - rect[1][1]
+                y1 = box[0][1]
+                x0 = box[0][0]
+                x1 = box[0][0] + rect[1][0]
+                cropImg = RotImg[int(y0):int(y1), int(x0):int(x1)]
+                cv2.imwrite(desrc + str(num) +str(idx) + '_' + jsonfile['shapes'][idx]['label'] + '.jpg', cropImg)
+
+if __name__ == '__main__':
+    for i, filename in tqdm(enumerate(jsonList)):
+        align(i,filename)
+    # align(100, '1520921045284-jueyuanzi.json')
+
